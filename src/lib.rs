5ed47cdaa61f139250893f3d87853a4aa1d623d5
fBurde <burde@nbs-hamburg.de>
2023-08-08 16:40:00 +0200

    added test suite // added ignore_case env var and cli argument // errors -> stderr

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,67 @@
 use std::fs;
 use std::error::Error;
+use std::env;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn case_sensitive(){
+        let query = "duct";
+        let contents = "\
+        Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."],search(query,contents));
+    }
+
+    #[test]
+    fn case_insensitive(){
+        let query: &str = "HaYsTaCk";
+        let contents: &str = "\
+        testline \nNeedle in a haystack\nyoubehaysacking";
+        assert_eq!(vec!["Needle in a haystack"],search_case_insensitive(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str,contents: &'a str) -> Vec<&'a str> {
+    let mut lines_found = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            lines_found.push(line);
+        }
+    }
+    lines_found
+}
+
+pub fn search_case_insensitive<'a>(query: &str,contents: &'a str) -> Vec<&'a str>{
+    let mut lines_found = Vec::new();
+    let lower_query = query.to_lowercase();
+    for line in contents.lines(){
+        if line.to_lowercase().contains(&lower_query){
+            lines_found.push(line);
+        }
+    }
+    lines_found
+}
+
 
 pub fn run(config: Config) -> Result<(),Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
     
-    println!("Test: \n{contents}");
+    for line in results {
+        println!("{line}");
+    }
+    // better implementation Lifetime ends here with code below
+    //
+    // for line in search(&config.query, &contents) {
+    //   println!("{line}");
+    //}
     Ok(())
 
 }
@@ -12,6 +69,7 @@ pub fn run(config: Config) -> Result<(),Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -19,8 +77,24 @@ impl Config {
         if args.len() < 3 {
             return Err("not enough arguments provided need 1.querystring 2.filepath");
         }
+        let add_arguments = &args[3..];
+        let mut ignore_case = env::var("IGNORE_CASE").is_ok();
+        for arg in add_arguments {
+            match &arg {
+                &s if s == "--i" =>  {
+                    ignore_case = true;
+                },
+                _s => println!("No Arguments found"),
+
+            }
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path } )
+        
+        Ok(Config { 
+             query,
+             file_path,
+             ignore_case
+        })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err|{
-        println!("Problem while parsing config data: {err}");
+        eprintln!("Problem while parsing config data: {err}");
         process::exit(1);
     });
-    println!("In file {}",config.query);
     if let Err(e) = mini_grep::run(config) {
-        println!("Encountered an Error while running the Application: {e}");
+        eprintln!("Encountered an Error while running the Application: {e}");
         process::exit(1);
     }
     
